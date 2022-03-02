64f5f9d7ada9578c368916db2d8bdea2516e0279
Willian <willian@pop-os.localdomain>
2022-03-01 22:21:56 -0300

    primal commit

--- rotas_basicas/app.py
+++ rotas_basicas/app.py
@@ -1,1 +1,26 @@
-# Seu código aqui
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.get('/')
+def home():
+    return {"data": "Hello Flask!"}
+
+
+@app.get("/current_datetime")
+def current_datetime():
+    msg=""
+    time = datetime.now()
+    hours = time.strftime("%H")
+    hours=int(hours)
+    if hours < 12:  
+        msg="Bom dia!"
+    if hours >= 12 and hours < 18:
+        msg ="Boa tarde!"
+    if hours >= 18:
+        msg = "Boa noite!"
+
+    return {"current_datetime":f'{time.strftime("%d/%m/%y %I:%M:%S %p")}',
+    "message":f'{msg}'}    
+
